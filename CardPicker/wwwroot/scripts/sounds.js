9b79ef5bd0534910da735c766d04bc5c20af3777
Dvorszki Dániel <19471620+Th3B1gB4dW0lf@users.noreply.github.com>
2022-03-31 22:50:33 +0200

    Test
    Base structure for testing

---
+++ CardPicker/wwwroot/scripts/sounds.js
@@ -0,0 +1,8 @@
+﻿window.PlayAudio = (elementName) => {
+    document.getElementById(elementName).play();
+}
+
+function DrawCard(imagePath) {
+    document.getElementById("card").src = imagePath;
+}
+
