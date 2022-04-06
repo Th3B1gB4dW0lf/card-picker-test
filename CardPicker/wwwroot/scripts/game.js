2d29e303cbaa8a640bf8bdeb55b057561e3cc126
Dvorszki Dániel <19471620+Th3B1gB4dW0lf@users.noreply.github.com>
2022-04-06 21:37:24 +0200

    Small enhancements
    - Add new uroboros and glow image
    - Make the image loading faster
    - Adjust audio playing

---
+++ CardPicker/wwwroot/scripts/game.js
@@ -0,0 +1,17 @@
+﻿window.PlayAudio = (elementName) => {
+    document.getElementById(elementName).play();
+}
+
+window.StopAudio = (elementName) => {
+    let audioElement = document.getElementById(elementName);
+    audioElement.pause();
+    audioElement.currentTime = 0;
+}
+
+window.HideElement = (elementName) => {
+    document.getElementById(elementName).classList.add("hide-element");
+}
+
+window.ShowElement = (elementName) => {
+    document.getElementById(elementName).classList.remove("hide-element");
+}
