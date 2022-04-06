bd2306ee1065e4aa29250163a9a7b8827f0df0e1
Dvorszki Dániel <19471620+Th3B1gB4dW0lf@users.noreply.github.com>
2022-04-06 22:03:45 +0200

    Performance enhancement

--- CardPicker/wwwroot/scripts/game.js
+++ CardPicker/wwwroot/scripts/game.js
@@ -15,3 +15,21 @@ window.HideElement = (elementName) => {
 window.ShowElement = (elementName) => {
     document.getElementById(elementName).classList.remove("hide-element");
 }
+
+window.StartWaiting = () => {
+    document.getElementById("uroboros-glow").classList.remove("hide-element");
+    document.getElementById("uroboros").classList.remove("hide-element");
+    document.getElementById("waiting-sound-effect").play();
+}
+
+window.PresentCard = () => {
+    document.getElementById("uroboros").classList.add("hide-element");
+    document.getElementById("uroboros-glow").classList.add("hide-element");
+
+    let audioElement = document.getElementById("waiting-sound-effect");
+    audioElement.pause();
+    audioElement.currentTime = 0;
+
+    document.getElementById("drawed-card").classList.remove("hide-element");
+    document.getElementById("draw-sound-effect").play();
+}
